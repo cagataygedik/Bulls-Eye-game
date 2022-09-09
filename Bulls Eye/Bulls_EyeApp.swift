7e06b149dcf783ada3c52e46efdf4dae3c39cc97
cagatay <cagatay.gedik@icloud.com>
2022-09-09 11:20:45 +0300

    Initial Commit

---
+++ Bulls Eye/Bulls_EyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bulls_EyeApp.swift
+//  Bulls Eye
+//
+//  Created by Celil Çağatay Gedik on 9.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Bulls_EyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
