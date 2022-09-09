939d35a6b82370c79e71333e6f557d91e7c53613
cagatay <cagatay.gedik@icloud.com>
2022-09-09 13:50:28 +0300

    first commit/demo of the app

---
+++ Bulls Eye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bulls Eye
+//
+//  Created by Celil Çağatay Gedik on 9.09.2022.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 42
+    var score:  Int = 0
+    var round:  Int = 1
+    
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+}
---
+++ Bulls Eye/Views/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Bulls Eye
+//
+//  Created by Celil Çağatay Gedik on 9.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
+    
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .kerning(2.0)
+                .font(.footnote)
+                .fontWeight(.bold)
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .lineSpacing(4.0)
+            Text(String(game.target))
+                .font(.largeTitle)
+                .kerning(-1.0)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .fontWeight(.bold)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
+                Text("100")
+                    .fontWeight(.bold)
+            }
+            
+            //button
+            Button(action: {
+                print("Hello, SwiftUI")
+                self.alertIsVisible = true
+            })  {
+                Text("HIT ME")
+            }
+            
+            //pop-up button
+            .alert("Hello There!", isPresented: $alertIsVisible) {
+                Button("Awesome") {}
+            } message: {
+                let roundedValue = Int(sliderValue.rounded())
+                Text("The slider's value is \(roundedValue). \n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round.")
+            }
+            
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+        //added landscape mode
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
+    }
+}
