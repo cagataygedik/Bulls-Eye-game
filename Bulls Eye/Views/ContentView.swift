07329bcfea27d7c37adfa29cc00e5aba87425f0c
cagatay <cagatay.gedik@icloud.com>
2022-09-10 17:30:06 +0300

    Edited code and Improved UI

--- Bulls Eye/Views/ContentView.swift
+++ Bulls Eye/Views/ContentView.swift
@@ -14,43 +14,43 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
+        ZStack {
+            Color("Background Color")
+            VStack {
+                
+                //label & number
+                InstructionView(game: $game)
+                
+                //Slider
+                SliderView(sliderValue: $sliderValue)
+                
+                //Button and Pop-up
+                HitMeButtonView(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        
+    }
+}
+
+struct InstructionView : View {
+    @Binding var game : Game
+    
+    var body: some View {
+        
         VStack {
-            Text("🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .kerning(2.0)
-                .font(.footnote)
-                .fontWeight(.bold)
-                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .lineSpacing(4.0)
-            Text(String(game.target))
-                .font(.largeTitle)
-                .kerning(-1.0)
-                .fontWeight(.black)
-            HStack {
-                Text("1")
-                    .fontWeight(.bold)
-                Slider(value: self.$sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .fontWeight(.bold)
-            }
+            InstrucationText(text: "🎯🎯🎯\n PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
             
-            //button
-            Button(action: {
-                self.alertIsVisible = true
-            })  {
-                Text("HIT ME")
-            }
             
-            //pop-up button
-            .alert("Hello There!", isPresented: $alertIsVisible) {
-                Button("Awesome") {}
-            } message: {
-                let roundedValue = Int(sliderValue.rounded())
-                Text("The slider's value is \(roundedValue). \n" + "You scored \(self.game.points(sliderValue: roundedValue)) points this round.")
-            }
+            BigNumberText(text:     String(game.target))
+            
             
         }
     }
 }
+
 
 
 struct ContentView_Previews: PreviewProvider {
@@ -59,5 +59,13 @@ struct ContentView_Previews: PreviewProvider {
         
         //added landscape mode
         ContentView().previewLayout(.fixed(width: 568, height: 320))
+        
+        //dark mode
+        ContentView()
+            .preferredColorScheme(.dark)
+        
+        //added landscape mode
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
+            .preferredColorScheme(.dark)
     }
 }
