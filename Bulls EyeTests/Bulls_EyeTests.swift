9b57d862f0f38d2324ba71991f0b1089fd6cc78e
cagatay <cagatay.gedik@icloud.com>
2022-09-12 14:10:07 +0300

    added new tests

--- Bulls EyeTests/Bulls_EyeTests.swift
+++ Bulls EyeTests/Bulls_EyeTests.swift
@@ -32,7 +32,34 @@ class Bulls_EyeTests: XCTestCase {
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 95)
     }
+    
+    func testNewRound() {
+        game.startNewRound(points: 100)
+        XCTAssertEqual(game.score, 100)
+        XCTAssertEqual(game.round, 2)
+    }
+    
+    func testScoreExact() {
+        let guess = game.target
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 200)
+    }
 
+    func testScoreClose() {
+        let guess = game.target + 2
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 98 + 50)
+    }
+    
+    func testRestart() {
+        game.startNewRound(points: 100)
+        game.restart()
+        XCTAssertEqual(game.score, 0)
+        XCTAssertEqual(game.round, 1)
+
+        
+    }
+    
     }
 
 
